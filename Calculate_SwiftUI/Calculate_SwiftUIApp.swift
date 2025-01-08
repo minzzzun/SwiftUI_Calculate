87f7eb44adce733bf5babeb6d06cb980589e07cf
minjun kim <minzzzun@gmail.com>
2025-01-08 21:34:46 +0900

    계산기 앱 연습 시작

---
+++ Calculate_SwiftUI/Calculate_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculate_SwiftUIApp.swift
+//  Calculate_SwiftUI
+//
+//  Created by 김민준 on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Calculate_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculate_SwiftUITests/Calculate_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  Calculate_SwiftUITests.swift
+//  Calculate_SwiftUITests
+//
+//  Created by 김민준 on 1/8/25.
+//
+
+import Testing
+@testable import Calculate_SwiftUI
+
+struct Calculate_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
